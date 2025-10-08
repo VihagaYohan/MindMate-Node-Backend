f92830784b9d20e218c93fcc5b6235b80974ff82
Vihaga Yohan <vihagayohan94@gmail.com>
2025-10-08 07:47:41 +0100

    Added - get moods endpoint with pagination

--- src/controllers/userMoodController.ts
+++ src/controllers/userMoodController.ts
@@ -2,8 +2,59 @@ import { Request, Response, NextFunction } from 'express'
 import { AsyncHandler } from '../middleware'
 import { UserMoodSchema } from '../schemas'
 import { ErrorResponse, SuccessResponse } from '../shared/response'
+import { Types } from 'mongoose'
 
 const { UserMood, userMoodSchemaValidation } = UserMoodSchema
+
+/* 
+    @desc       Get all user moods
+    @routes     GET /api/v1/user-mood/:id
+    @access     Private
+*/
+export const getAllUserMoods = AsyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.user.id
+    // check for unauthorized user id
+    if (!userId) {
+        return next(new ErrorResponse(400, "Unauthorized access deined. user id not found"))
+    }
+
+    // extract query params
+    const page = parseInt(req.query.page as string, 10) || 1
+    const limit = parseInt(req.query.limi as string, 10) || 10
+
+    // handle optional date query
+    const dateParam = req.query.date ? new Date(Number(req.query.date)) : new Date();
+
+    // create date range (start and end of the given day)
+    const startOfDay = new Date(dateParam);
+    startOfDay.setHours(0, 0, 0, 0);
+
+    const endOfDay = new Date(dateParam);
+    endOfDay.setHours(23, 59, 59, 999);
+
+    // query filter
+    const filter = {
+        userId: new Types.ObjectId(userId),
+        createdAt: { $gte: startOfDay, $lte: endOfDay },
+        isActive: true
+    }
+
+    // count total results for pagination
+    const total = await UserMood.countDocuments(filter)
+
+    // fetch paginated results
+    const moods = await UserMood.find(filter).sort({ createdAt: -1 }).skip((page - 1) * limit).limit(limit)
+
+    const respondObj = {
+        currentPage: page,
+        totalPages: Math.ceil(total / limit),
+        totalItems: total,
+        data: moods
+    }
+
+    res.status(200).json(new SuccessResponse(true, "User mood fetch successfully", respondObj))
+
+})
 
 /* 
     @desc       Add user mood entry
--- src/routes/userMood.ts
+++ src/routes/userMood.ts
@@ -1,10 +1,10 @@
 import express from 'express'
-import { addUserMood, updateUserMood, deleteUserMood } from '../controllers/userMoodController'
+import { addUserMood, updateUserMood, deleteUserMood, getAllUserMoods } from '../controllers/userMoodController'
 import { AuthHandler } from '../middleware'
 
 const router = express.Router()
 
-router.route('/').post(AuthHandler, addUserMood)
+router.route('/').get(AuthHandler, getAllUserMoods).post(AuthHandler, addUserMood)
 
 router.route('/:id').put(AuthHandler, updateUserMood).delete(AuthHandler, deleteUserMood)
 
